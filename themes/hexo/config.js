a017873afcaf42ec0e1e72986b5155a27cbca260
caozijie <65272060+caozijie@users.noreply.github.com>
2023-09-12 11:37:12 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -11,9 +11,9 @@ const CONFIG = {
 
   // 菜单配置
   MENU_INDEX: true, // 显示首页
-  MENU_CATEGORY: true, // 显示分类
+  MENU_CATEGORY: false, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: false, // 显示搜索
 
   POST_LIST_COVER: true, // 列表显示文章封面
@@ -22,7 +22,7 @@ const CONFIG = {
   POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   POST_LIST_SUMMARY: true, // 文章摘要
   POST_LIST_PREVIEW: false, // 读取文章预览
-  POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  POST_LIST_IMG_CROSSOVER: false, // 博客列表图片左右交错
 
   ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   ARTICLE_COPYRIGHT: true, // 显示文章版权声明
